01c1eb4ca8a98f50ebc49a981afed7040038d4cd
Dmitrii Klochkov <d.klochkov9421@gmail.com>
2023-01-31 17:35:44 +0300

    Исправление мелких ошибок

--- Program.cs
+++ Program.cs
@@ -14,10 +14,8 @@ static void Main(string[] args)
     int arrayLength = testArray.Length;
 
     string[] tempArray = new string[testArray.Length];
-
-    int strLen2 = testArray.Length;
-
-    Console.WriteLine("Заполните массив данными. Внимание должнобыть хотябы 2 значения с длинной не больше 3 символов.");
+    
+    Console.WriteLine("Заполните массив данными. Внимание, должнобыть хотябы 2 значения с длинной не больше 3 символов.");
     Thread.Sleep(1000);
     for (int i = 0; i < arrayLength; i++)
     {
@@ -35,7 +33,6 @@ static void Main(string[] args)
         {
             Console.WriteLine("Загрузка");
         }
-
         else
         {
             if (i == 9)
@@ -50,34 +47,22 @@ static void Main(string[] args)
     Thread.Sleep(1000);
     Console.WriteLine();
 
-
-
     string[] tempArray2 = new string[arrayLength];
 
-    int k = 0;
-
     Console.WriteLine($"Запись произведина");
-
+    
     for (int j = 0; j < tempArray.Length; j++)
     {
 
         if (testArray[j].Length <= 3)
         {
-            int countStart = 0;
-            int countStop = 1;
-
-            while (countStart < countStop)
+            
+            for(int k = 0; k < 1;k++)
             {
                 tempArray2[k] = testArray[j];
-                Console.Write($" {tempArray2[k]}");
-                countStart++;
-                k++;
+                Console.Write($"{tempArray2[k]} ");
             }
-
         }
-
     }
 }
 Main(args);
-
-for()
